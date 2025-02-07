0843b98d9a4a2d4698e1baa87c9591e78a99042d
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-02-07 13:11:23 -0800

    Initial Commit

---
+++ GroceryList.java
@@ -0,0 +1,26 @@
+//import java.util.Map;
+
+public class GroceryList 
+{
+     //private Map<String, Boolean> groceries;
+     private String test;
+
+     public GroceryList()
+     {
+          //Setting up function
+          this.test = "test";
+     }
+
+     // Adding item
+     public void addTest(String newWord)
+     {
+          this.test = newWord;
+     }
+
+     // Print Item
+     public String printTest()
+     {
+          return test;
+     }
+
+}
